6791b0c7de9dd8a678582ad3c22e7180feb96b37
s20522 <81235106+s20522@users.noreply.github.com>
2025-05-08 22:38:20 +0200

    Add files via upload

---
+++ s20522_apbd_cw7_projekt/Program.cs
@@ -0,0 +1,29 @@
+using TravelAgencyApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Travel Agency API", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Travel Agency API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
