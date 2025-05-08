1f06cac5f95b1f6847a71c301221f972841b7c1f
s20522 <81235106+s20522@users.noreply.github.com>
2025-05-08 22:37:06 +0200

    Add files via upload

---
+++ s20522_apbd_cw7_projekt/Controllers/TripsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelAgencyApi.Services;
+using TravelAgencyApi.DTOs;
+
+namespace TravelAgencyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly IDatabaseService _dbService;
+
+        public TripsController(IDatabaseService dbService)
+        {
+            _dbService = dbService;
+        }
+        // Endpoint pobierający wszystkie dostępne wycieczki (podstawowe info + kraj)
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<TripResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetTrips()
+        {
+            try
+            {
+                var trips = await _dbService.GetTripsAsync();
+                return Ok(trips);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in GetTrips: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Wystąpił błąd serwera podczas pobierania wycieczek.");
+            }
+        }
+    }
+}
