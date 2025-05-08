9aabfc2a4b6a41a5aa405de608baccb38b85b7e8
s20522 <81235106+s20522@users.noreply.github.com>
2025-05-08 22:44:18 +0200

    Add files via upload

---
+++ s20522_apbd_cw7_projekt/Services/IDatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using TravelAgencyApi.DTOs;
+using TravelAgencyApi.Models;
+
+namespace TravelAgencyApi.Services
+{
+    public interface IDatabaseService
+    {
+        Task<IEnumerable<TripResponseDto>> GetTripsAsync();
+        Task<Client?> GetClientByIdAsync(int idClient);
+        Task<IEnumerable<ClientTripResponseDto>> GetClientTripsAsync(int idClient);
+        Task<bool> ClientExistsAsync(int idClient);
+        Task<bool> TripExistsAsync(int idTrip);
+        Task<Trip?> GetTripByIdAsync(int idTrip);
+        Task<bool> IsClientRegisteredForTripAsync(int idClient, int idTrip);
+        Task<int> GetCurrentTripParticipantCountAsync(int idTrip);
+        Task<int> AddClientAsync(ClientCreateRequestDto clientDto);
+        Task<bool> AssignClientToTripAsync(int idClient, int idTrip);
+        Task<bool> DeleteClientTripAssignmentAsync(int idClient, int idTrip);
+        Task<bool> ClientWithPeselExistsAsync(string pesel);
+        Task<bool> ClientWithEmailExistsAsync(string email);
+    }
+}
